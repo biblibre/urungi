a69bcb29344b04976f22747484cb295ea494b653
hromero@db-team.com <hromero@db-team.com>
2017-05-19 13:34:47 +0100

    Oracle connection fix

--- server/core/db/oracle.js
+++ server/core/db/oracle.js
@@ -41,15 +41,20 @@ db.prototype.query = function(query, done) {
             return console.error('Connection Error: '+err);
         }
 
-        result.rows = columnNamesToLowerCase(result.rows);
-
+        //result.rows = columnNamesToLowerCase(result.rows);
         done(false, {rows: result.rows});
     });
 };
-
+/*
 db.prototype.getSchemaQuery = function(newSchemas, newTables) {
     return "SELECT t.tablespace_name as table_schema, c.table_name, c.column_name, c.data_type"+
     " FROM user_tab_columns c, user_tables t WHERE t.tablespace_name in ("+newSchemas+") AND c.table_name IN ("+newTables+")"+
+    " AND t.table_name = c.table_name";
+};
+*/
+db.prototype.getSchemaQuery = function(newSchemas, newTables) {
+    return "SELECT t.owner as table_schema, c.table_name, c.column_name, LOWER(c.data_type) as data_type"+
+    " FROM user_tab_columns c, all_tables t WHERE t.owner in ("+newSchemas+") AND c.table_name IN ("+newTables+")"+
     " AND t.table_name = c.table_name";
 };
 
@@ -62,7 +67,7 @@ db.prototype.setLimitToSQL = function(sql,limit,offset)
    if (limit == -1)
         return sql
        else
-    return ' SELECT * FROM (SELECT rownum rnum, a.* FROM('+sql+') a WHERE rownum <='+offset+'+'+limit+') WHERE rnum >='+offset
+        return ' SELECT * FROM (SELECT rownum wst_rnum, a.* FROM('+sql+') a WHERE rownum <='+offset+'+'+limit+') WHERE wst_rnum >='+offset
 
 }
 
@@ -83,8 +88,10 @@ exports.testConnection = function(req,data, setresult) {
 
         console.log('Connected to '+data.database+' getting table names');
 
-        connection.execute("SELECT tablespace_name as table_schema, table_name as name FROM user_tables", [], {outFormat: oracledb.OBJECT}, function(err, result) {
-            result.rows = columnNamesToLowerCase(result.rows);
+        var theQuery = 'SELECT user as table_schema, table_name as name FROM user_tables';
+
+        connection.execute(theQuery, [], {outFormat: oracledb.OBJECT}, function(err, result) {
+            //result.rows = columnNamesToLowerCase(result.rows);
 
 
             setresult({result: 1, items: result.rows});
