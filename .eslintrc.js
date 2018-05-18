cf7317a3cced5e8f38ffba55e8b65aef1c53ab64
Julian Maurice <julian.maurice@biblibre.com>
2018-05-18 18:49:32 +0200

    Enforce JS style with ESLint and EditorConfig

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    extends: 'standard',
+    env: {
+        jquery: true,
+    },
+    globals: {
+        // defined in server.js
+        config: false,
+
+        // defined in server/config/mongoose.js
+        connection: false,
+
+        // defined in server/core/controller.js
+        Controller: false,
+
+        // defined in server/globals.js
+        appRoot: false,
+        debug: false,
+        generateUserFilter: false,
+        generateUserFilterValue: false,
+        getNextSequence: false,
+        isAllowed: false,
+        passthrough: false,
+        restrict: false,
+        restrictRole: false,
+        saveToLog: false,
+        sendEmailTemplate: false,
+        sendCommunication: false,
+        sendNotification: false,
+        serverResponse: false,
+
+        // Browser
+        angular: false,
+        app: false,
+    },
+    rules: {
+        'no-console': 'off',
+        'camelcase': 'off',
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'semi': ['error', 'always'],
+        'comma-dangle': ['error', 'only-multiline'],
+        'prefer-const': 'error',
+    }
+};
