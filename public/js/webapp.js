a2004be6374ae84cc8e8a2c61646140e6fb22a66
Julian Maurice <julian.maurice@biblibre.com>
2018-10-22 23:56:13 +0200

    Remove unused dependency angular-csv-import

--- public/js/webapp.js
+++ public/js/webapp.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var app = angular.module('Urungi', [
-    'ngRoute', 'ui.sortable', 'draganddrop', 'ui.bootstrap', 'ngCsvImport',
+    'ngRoute', 'ui.sortable', 'draganddrop', 'ui.bootstrap',
     'infinite-scroll', 'ui.bootstrap-slider', 'urungi.directives', 'ngSanitize', 'ui.select', 'angularUUID2', 'vs-repeat',
     'ui.bootstrap.datetimepicker', 'ui.tree', 'page.block', 'bsLoadingOverlay', 'xeditable',
     'intro.help', 'ngTagsInput', 'ui.codemirror', 'ngFileUpload', 'colorpicker.module',
