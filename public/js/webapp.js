dc63d0aa3bff4aa9198c2e72c41a62130858bfd8
Julian Maurice <julian.maurice@biblibre.com>
2019-05-16 19:00:32 +0200

    Use explicit dependency injection annotations

--- public/js/webapp.js
+++ public/js/webapp.js
@@ -294,13 +294,13 @@ angular.module('app').run(['$rootScope', 'sessionStorage', 'connection', functio
     }
 }]);
 
-angular.module('app').run(function (bsLoadingOverlayService) {
+angular.module('app').run(['bsLoadingOverlayService', function (bsLoadingOverlayService) {
     bsLoadingOverlayService.setGlobalConfig({
         delay: 0, // Minimal delay to hide loading overlay in ms.
         activeClass: undefined, // Class that is added to the element where bs-loading-overlay is applied when the overlay is active.
         templateUrl: 'partials/loading-overlay-template.html' // Template url for overlay element. If not specified - no overlay element is created.
     });
-});
+}]);
 
 // Set default options for xeditable
 angular.module('app').run(['editableOptions', function (editableOptions) {
