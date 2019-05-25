0c32994944d2ee4723bd40166196e3ce5ddfdf5f
Julian Maurice <julian.maurice@biblibre.com>
2019-05-25 13:34:45 +0200

    Replace custom directive ngEnter by ngSubmit

--- public/js/webapp.js
+++ public/js/webapp.js
@@ -104,20 +104,6 @@ angular.module('app', [
         });
     }]);
 
-angular.module('app').directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind('keydown keypress', function (event) {
-            if (event.which === 13) {
-                scope.$apply(function () {
-                    scope.$eval(attrs.ngEnter, { 'event': event });
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
-});
-
 angular.module('app').service('reportService', function () {
     var theReport = {};
 
