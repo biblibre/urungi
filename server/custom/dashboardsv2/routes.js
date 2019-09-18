46ac5193d4c735260db665857fcf48b5c270af59
Julian Maurice <julian.maurice@biblibre.com>
2019-09-18 11:59:52 +0200

    Add ability to export reports and dashboards as PNG or PDF
    It uses Pikitia (https://github.com/biblibre/pikitia) to take a
    screenshot or generate a PDF

--- server/custom/dashboardsv2/routes.js
+++ server/custom/dashboardsv2/routes.js
@@ -1,4 +1,6 @@
+const config = require('config');
 const restrict = require('../../middlewares/restrict');
+const pikitia = require('../../helpers/pikitia');
 
 module.exports = function (app) {
     var Dashboardsv2 = require('./controller.js');
@@ -15,4 +17,55 @@ module.exports = function (app) {
     app.post('/api/dashboardsv2/unpublish', restrict, Dashboardsv2.UnpublishDashboard);
     app.post('/api/dashboardsv2/share-page', restrict, Dashboardsv2.ShareDashboard);
     app.post('/api/dashboardsv2/unshare', restrict, Dashboardsv2.UnshareDashboard);
+
+    app.get('/api/dashboards/:id.png', async function (req, res, next) {
+        const mongoose = require('mongoose');
+        const Dashboard = mongoose.model('Dashboardsv2');
+
+        try {
+            const dashboard = await Dashboard.findById(req.params.id);
+            if (!(dashboard && (req.isAuthenticated() || dashboard.isPublic))) {
+                return res.sendStatus(404);
+            }
+
+            const url = config.get('url') + `/#/dashboards/view/${dashboard.id}`;
+            const buffer = await pikitia.toPNG(url, {
+                cookies: req.cookies,
+                viewport: {
+                    width: 1920,
+                    height: 1080,
+                },
+            });
+
+            const filename = dashboard.dashboardName.replace(/"/g, '_') + '.png';
+
+            res.set('Content-Disposition', 'attachment; filename="' + filename + '"');
+            res.set('Content-Type', 'image/png');
+            res.status(200).send(buffer);
+        } catch (e) {
+            return next(e);
+        }
+    });
+
+    app.get('/api/dashboards/:id.pdf', async function (req, res, next) {
+        const mongoose = require('mongoose');
+        const Dashboard = mongoose.model('Dashboardsv2');
+
+        try {
+            const dashboard = await Dashboard.findById(req.params.id);
+            if (!(dashboard && (req.isAuthenticated() || dashboard.isPublic))) {
+                return res.sendStatus(404);
+            }
+
+            const url = config.get('url') + `/#/dashboards/view/${dashboard.id}`;
+            const buffer = await pikitia.toPDF(url, { cookies: req.cookies });
+
+            const filename = dashboard.dashboardName.replace(/"/g, '_') + '.pdf';
+            res.set('Content-Disposition', 'attachment; filename="' + filename + '"');
+            res.set('Content-Type', 'application/pdf');
+            res.status(200).send(buffer);
+        } catch (e) {
+            return next(e);
+        }
+    });
 };
--- server/custom/reports/routes.js
+++ server/custom/reports/routes.js
@@ -1,4 +1,8 @@
+const config = require('config');
 const restrict = require('../../middlewares/restrict');
+const mongoose = require('mongoose');
+const Report = mongoose.model('Reports');
+const pikitia = require('../../helpers/pikitia');
 
 module.exports = function (app) {
     var Reports = require('./controller.js');
@@ -15,4 +19,48 @@ module.exports = function (app) {
     app.post('/api/reports/unpublish', restrict, Reports.UnpublishReport);
     app.post('/api/reports/share-report', restrict, Reports.ShareReport);
     app.post('/api/reports/unshare', restrict, Reports.UnshareReport);
+
+    app.get('/api/reports/:id.png', async function (req, res, next) {
+        try {
+            const report = await Report.findById(req.params.id);
+            if (!(report && (req.isAuthenticated() || report.isPublic))) {
+                return res.sendStatus(404);
+            }
+
+            const url = config.get('url') + `/#/reports/view/${report.id}`;
+            const buffer = await pikitia.toPNG(url, {
+                cookies: req.cookies,
+                viewport: {
+                    width: 1920,
+                    height: 1080,
+                },
+            });
+
+            const filename = report.reportName.replace(/"/g, '_') + '.png';
+            res.set('Content-Disposition', 'attachment; filename="' + filename + '"');
+            res.set('Content-Type', 'image/png');
+            res.status(200).send(buffer);
+        } catch (e) {
+            return next(e);
+        }
+    });
+
+    app.get('/api/reports/:id.pdf', async function (req, res, next) {
+        try {
+            const report = await Report.findById(req.params.id);
+            if (!(report && (req.isAuthenticated() || report.isPublic))) {
+                return res.sendStatus(404);
+            }
+
+            const url = config.get('url') + `/#/reports/view/${report.id}`;
+            const buffer = await pikitia.toPDF(url, { cookies: req.cookies });
+
+            const filename = report.reportName.replace(/"/g, '_') + '.pdf';
+            res.set('Content-Disposition', 'attachment; filename="' + filename + '"');
+            res.set('Content-Type', 'application/pdf');
+            res.status(200).send(buffer);
+        } catch (e) {
+            return next(e);
+        }
+    });
 };
