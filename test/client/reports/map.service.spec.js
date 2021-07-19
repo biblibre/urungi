fcc9922104e231a08f8cff0fac1d024162435122
Thibaud Guillot <thibaud.guillot@biblibre.com>
2021-07-19 15:32:26 +0000

    WIP tests on map service

--- test/client/reports/map.service.spec.js
+++ test/client/reports/map.service.spec.js
@@ -28,38 +28,92 @@ describe('map', function () {
             mapDiv.style.height = '300px';
             mapDiv.style.width = '300px';
             document.body.append(mapDiv);
-
             const report = {
-                selectedLayerID: 'foo',
                 properties: {
-                    ykeys: [
-                        {
-                            label: 'Latitude',
-                            elementID: 'quux',
+                    map: {
+                        geojson: [{
+                            id: 'eaiufraw',
+                            elementID: 'aiuf',
+                            elementName: 'comp',
+                            elementLabel: 'geojson'
+                        }],
+                        group: [{
+                            id: 'eagmeraw',
+                            elementName: 'gid',
+                            elementLabel: 'grid',
+                            elementType: 'number',
+                            elementID: 'agme'
+                        }],
+                        label: [{
+                            id: 'eafrsraw',
+                            elementName: 'label',
+                            elementLabel: 'label',
                             elementType: 'string',
-                            id: 'equuxraw',
-                        },
-                        {
-                            label: 'Longitude',
-                            elementID: 'quuz',
+                            elementID: 'afrs'
+                        }],
+                        type: [{
+                            id: 'eahaoraw',
+                            elementName: 'type',
+                            elementLabel: 'type',
                             elementType: 'string',
-                            id: 'equuzraw',
-                        },
-                    ],
+                            elementID: 'ahao'
+                        }],
+                        value: [{
+                            id: 'eaexaraw',
+                            elementName: 'value',
+                            elementLabel: 'value',
+                            elementType: 'number',
+                            elementID: 'aexa'
+                        }],
+                    }
                 }
             };
-            const rows = [
-                {
-                    equuxraw: '18.05',
-                    equuzraw: '5.46',
-                },
-                {
-                    equuxraw: '13.67',
-                    equuzraw: '2.64',
-                },
+
+            const dataRows = [{
+                eaexaraw: 900,
+                eafrsraw: 'Gap',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [1.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 100,
+                eafrsraw: 'Marseille',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [4.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 500,
+                eafrsraw: 'Paris',
+                eagmeraw: 2,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [9.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Lyon',
+                eagmeraw: 1,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [0.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 1,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 3,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
             ];
 
-            const m = map.createMap(report, rows);
+            const m = map.createMap(report, dataRows);
 
             const markers = [];
             m.eachLayer(function (layer) {
@@ -71,6 +125,256 @@ describe('map', function () {
             expect(markers).toHaveLength(2);
             expect(markers[0].getLatLng()).toStrictEqual({ lat: 18.05, lng: 5.46 });
             expect(markers[1].getLatLng()).toStrictEqual({ lat: 13.67, lng: 2.64 });
+        });
+    });
+    describe('getGroups', function () {
+        it('should add group to groups', function () {
+            const groups = new Set();
+
+            const dataRows = [{
+                eaexaraw: 900,
+                eafrsraw: 'Gap',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [1.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 100,
+                eafrsraw: 'Marseille',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [4.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 500,
+                eafrsraw: 'Paris',
+                eagmeraw: 2,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [9.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Lyon',
+                eagmeraw: 1,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [0.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 1,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            ];
+
+            for (const row of dataRows) {
+                const group = row.eagmeraw;
+                groups.add(group);
+            }
+
+            expect(groups.size).toStrictEqual(2);
+            expect(groups.has(2)).toStrictEqual(true);
+            expect(groups.has(1)).toStrictEqual(true);
+        });
+    });
+
+    describe('getGroupColor', function () {
+        it('should attribuate groups color', function () {
+            const groups = new Set();
+
+            const dataRows = [{
+                eaexaraw: 900,
+                eafrsraw: 'Gap',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [1.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 100,
+                eafrsraw: 'Marseille',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [4.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 500,
+                eafrsraw: 'Paris',
+                eagmeraw: 2,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [9.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Lyon',
+                eagmeraw: 1,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [0.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 1,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            {
+                eaexaraw: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 3,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            ];
+
+            for (const row of dataRows) {
+                const group = row.eagmeraw;
+                groups.add(group);
+            }
+
+            const groupHue = new Map();
+
+            let hueIndex = 0;
+
+            const groupCount = groups.size;
+
+            for (const group of groups) {
+                const hue = hueIndex * (360 / groupCount);
+                hueIndex++;
+                groupHue.set(group, hue);
+            }
+
+            expect(groupHue.size).toStrictEqual(3);
+            expect(groupHue.has(1)).toStrictEqual(true);
+            expect(groupHue.has(2)).toStrictEqual(true);
+            expect(groupHue.has(3)).toStrictEqual(true);
+        });
+    });
+    describe('getStyle', function () {
+        it('should apply color style into a value range', function () {
+            const minValue = 20;
+            const maxValue = 100;
+            const value = 70;
+
+            const getStyle = function (value) {
+                const saturation = ((value - minValue) * 100) / (maxValue - minValue);
+                return saturation;
+            };
+
+            expect(value).toBeGreaterThanOrEqual(minValue);
+            expect(value).toBeLessThanOrEqual(maxValue);
+            expect(getStyle(70)).toStrictEqual(62.5);
+            expect(getStyle(50)).toStrictEqual(37.5);
+            expect(getStyle(25)).toStrictEqual(6.25);
+        });
+    });
+    describe('getValuesBounds', function () {
+        it('should get min & max value for apply color instensity', function () {
+            const colorIntensity = [];
+            const boundsValues = {};
+
+            const getValuesBounds = function (datas) {
+                datas.forEach((e) => colorIntensity.push(e.value));
+                boundsValues.minValue = Math.min(...colorIntensity);
+                boundsValues.maxValue = Math.max(...colorIntensity);
+
+                return boundsValues;
+            };
+
+            const dataRows = [{
+                value: 900,
+                eafrsraw: 'Gap',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [1.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 100,
+                eafrsraw: 'Marseille',
+                eagmeraw: 2,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [4.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 500,
+                eafrsraw: 'Paris',
+                eagmeraw: 2,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [9.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Lyon',
+                eagmeraw: 1,
+                eahaoraw: 'client',
+                eaiufraw: '{"type": "Point", "coordinates": [0.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 1,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Aix',
+                eagmeraw: 3,
+                eahaoraw: 'library',
+                eaiufraw: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            ];
+
+            const result = getValuesBounds(dataRows);
+            expect(result.maxValue).toStrictEqual(900);
+            expect(result.minValue).toStrictEqual(100);
+        });
+    });
+    describe('addMarkersToMap', function () {
+        it('should add markers on map', function () {
+            const dataRows = [{
+                value: 900,
+                eafrsraw: 'Gap',
+                group: 2,
+                eahaoraw: 'client',
+                geojson: '{"type": "Point", "coordinates": [1.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 100,
+                eafrsraw: 'Marseille',
+                group: 2,
+                eahaoraw: 'client',
+                geojson: '{"type": "Point", "coordinates": [4.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 500,
+                eafrsraw: 'Paris',
+                group: 2,
+                eahaoraw: 'library',
+                geojson: '{"type": "Point", "coordinates": [9.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Lyon',
+                group: 1,
+                eahaoraw: 'client',
+                geojson: '{"type": "Point", "coordinates": [0.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Aix',
+                group: 1,
+                eahaoraw: 'library',
+                geojson: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            {
+                value: 350,
+                eafrsraw: 'Aix',
+                group: 3,
+                eahaoraw: 'library',
+                geojson: '{"type": "Point", "coordinates": [10.3291015625, 28.90083790234091]}'
+            },
+            ];
         });
     });
 });
