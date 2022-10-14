a00dfd4a2c8da270deafb95f4c225850a686168e
Julian Maurice <julian.maurice@biblibre.com>
2022-10-14 13:37:51 +0200

    Factorize db migration code

--- migrations/20221012101241-update-filter-criteria-names.js
+++ migrations/20221012101241-update-filter-criteria-names.js
@@ -1,49 +1,25 @@
 module.exports = {
     async up (db) {
-        const reports = await db.collection('reports').find({}).toArray();
+        const reports = await db.collection('reports').find().toArray();
         for (const report of reports) {
-            for (const filter of report.properties.filters) {
-                if (filter.criterion.date1) {
-                    filter.criterion.text1 = filter.criterion.date1;
-                }
-                if (filter.criterion.date2) {
-                    filter.criterion.text2 = filter.criterion.date2;
-                }
-                delete filter.criterion.date1;
-                delete filter.criterion.date2;
-            }
+            fixReport(report);
             await db.collection('reports').replaceOne({ _id: report._id }, report);
         }
 
-        const dashboards = await db.collection('dashboards').find({}).toArray();
+        const dashboards = await db.collection('dashboards').find().toArray();
         for (const dashboard of dashboards) {
             const reports = dashboard.reports;
             for (const report of reports) {
-                const reportFilters = report.properties.filters;
-                for (const reportFilter of reportFilters) {
-                    if (reportFilter.criterion.date1) {
-                        reportFilter.criterion.text1 = reportFilter.criterion.date1;
-                    }
-                    if (reportFilter.criterion.date2) {
-                        reportFilter.criterion.text2 = reportFilter.criterion.date2;
-                    }
-                    delete reportFilter.criterion.date1;
-                    delete reportFilter.criterion.date2;
-                }
+                fixReport(report);
             }
             await db.collection('dashboards').replaceOne({ _id: dashboard._id }, dashboard);
         }
     },
 
     async down (db) {
-        const reports = await db.collection('reports').find({ 'properties.filters': { $elemMatch: { elementType: 'date' } } }).toArray();
+        const reports = await db.collection('reports').find({ 'properties.filters.elementType': 'date' }).toArray();
         for (const report of reports) {
-            for (const filter of report.properties.filters) {
-                if (filter.elementType === 'date') {
-                    filter.criterion.date1 = filter.criterion.text1;
-                    filter.criterion.date2 = filter.criterion.text2;
-                }
-            }
+            unfixReport(report);
             await db.collection('reports').replaceOne({ _id: report._id }, report);
         }
 
@@ -51,15 +27,33 @@ module.exports = {
         for (const dashboard of dashboards) {
             const reports = dashboard.reports;
             for (const report of reports) {
-                const reportFilters = report.properties.filters;
-                for (const reportFilter of reportFilters) {
-                    if (reportFilter.elementType === 'date') {
-                        reportFilter.criterion.date1 = reportFilter.criterion.text1;
-                        reportFilter.criterion.date2 = reportFilter.criterion.text2;
-                    }
-                }
+                unfixReport(report);
             }
             await db.collection('dashboards').replaceOne({ _id: dashboard._id }, dashboard);
         }
     }
 };
+
+function fixReport (report) {
+    for (const filter of report.properties.filters) {
+        if (filter.criterion.date1) {
+            filter.criterion.text1 = filter.criterion.date1;
+        }
+        if (filter.criterion.date2) {
+            filter.criterion.text2 = filter.criterion.date2;
+        }
+        delete filter.criterion.date1;
+        delete filter.criterion.date2;
+    }
+}
+
+function unfixReport (report) {
+    for (const filter of report.properties.filters) {
+        if (filter.elementType === 'date') {
+            filter.criterion.date1 = filter.criterion.text1;
+            filter.criterion.date2 = filter.criterion.text2;
+            delete filter.criterion.text1;
+            delete filter.criterion.text2;
+        }
+    }
+}
